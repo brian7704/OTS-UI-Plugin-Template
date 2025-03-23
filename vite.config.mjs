bf472b9867c43a4e52489cc14102066f7e77c440
Brian Wallen <bwallen@gmail.com>
2025-03-23 18:53:54 -0400

    Changed the build output dir

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,4 +9,8 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
   },
+  build: {
+    outDir: '../ots_plugin_template/ui',
+    emptyOutDir: true,
+  }
 });
