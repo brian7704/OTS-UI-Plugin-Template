bf00f38321d24572a85210d40809ec8cc2454893
Brian Wallen <bwallen@gmail.com>
2025-03-23 19:30:38 -0400

    Added some comments

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <HomePage />,
   },
-]);
+], { basename: "/api/plugins/ots_plugin_template" }); // <-------- TODO: Change this line
 
 export function Router() {
   return <RouterProvider router={router} />;
--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     setupFiles: './vitest.setup.mjs',
   },
   build: {
-    outDir: '../ots_plugin_template/ui',
+    outDir: '../ots_plugin_template/ui', // <-------- TODO: Change this line
     emptyOutDir: true,
   }
 });
